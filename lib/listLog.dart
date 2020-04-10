d0396bc9ab7537d75bc014abba15859dd2424b6f
augustineaykara <augustineaykara@gmail.com>
2020-04-10 23:45:19 +0530

    upload via git

--- lib/listLog.dart
+++ lib/listLog.dart
@@ -8,22 +8,53 @@ class ListLog extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: ListLogDetails(),      
+      home: ListLogDetails(),
     );
   }
 }
 
 class ListLogDetails extends StatefulWidget {
+  final String empId;
+  ListLogDetails({Key key, this.empId}) : super(key: key);
   @override
   _ListLogDetailsState createState() => _ListLogDetailsState();
 }
 
 class _ListLogDetailsState extends State<ListLogDetails> {
+
   @override
   Widget build(BuildContext context) {
+
+    Stream<QuerySnapshot> employeeSnapshot = Firestore.instance
+        .collection('employeeData')
+        .document('${widget.empId}')
+        .collection('logDetails')
+        .snapshots();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Log Details"),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: employeeSnapshot,
+        builder:
+           (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot)  {
+          if (!snapshot.hasData)
+            return Center(child: Text("Loading..."));
+          else {
+            return ListView(
+              children: snapshot.data.documents.map((DocumentSnapshot document) {
+                return ListTile(
+                  leading: Icon(Icons.access_time, color: Colors.deepOrange,),
+                  title: Text(document['Date & Time']),
+                  onTap: (){
+                    print("detailed view");
+                  },
+                );
+              }).toList(),
+            );
+          }
+        },
       ),
     );
   }
